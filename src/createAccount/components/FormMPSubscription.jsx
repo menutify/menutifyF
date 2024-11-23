e476b211a5a52b587e2b9e9fdd9b7a16d7212e2e
elkake <gianco.marquez@gmail.com>
2024-11-23 12:10:01 -0500

    stripe && mp

--- src/createAccount/components/FormMPSubscription.jsx
+++ src/createAccount/components/FormMPSubscription.jsx
@@ -1,144 +1,47 @@
-import { CardPayment } from '@mercadopago/sdk-react'
-import { useEffect, useState } from 'react'
-import axiosInstance from '../../utils/axiosConfig.js'
-import { useDataGlobalContext } from '../../Context/GlobalContext.jsx'
-import { v4 } from 'uuid'
-/**
- * probar pago:
- *   5287 3383 1025 3304
- *   11/25
- *   123
- */
+import {
+  CardNumber,
+  SecurityCode,
+  ExpirationDate,
+  createCardToken
+} from '@mercadopago/sdk-react'
 
 function FormMPSubscription() {
-  const [referenceId, setreferenceId] = useState(null)
-  const [idepotency, setIdepotency] = useState('')
-  const { user, product } = useDataGlobalContext()
+  const onSubmit = async (e) => {
+    e.preventDefault()
 
-  // useEffect(() => {
-  //   axiosInstance
-  //     .post('/payment/createReferenceMP')
-  //     .then(({ data }) => {
-  //       const id = data.data.id
-  //       setreferenceId(id)
-  //     })
-  //     .catch(console.log)
-  // }, [])
+    const response = await createCardToken({
+      cardholderName: 'APRO',
+      identificationType: 'DNI',
+      identificationNumber: '12345678'
+    })
+    console.log('Card Token Response = ', { response })
 
-  useEffect(() => {
-    if (window.cardPaymentBrickController) {
-      console.log('mp initialized')
-    }
-    const idempotencyKey = v4()
-    setIdepotency(idempotencyKey)
-    return () => {
-      console.log('mp unmounted')
-      // Si tienes un controlador explícito para desmontar el Brick:
-      if (window.cardPaymentBrickController) {
-        window.cardPaymentBrickController.unmount()
-      }
-    }
-  }, [])
+    // try {
+    //   const resp = await fetch('http://localhost:3000/api/payment/sub-intent', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       Authorization:
+    //         'Bearer TEST-6089233700666068-112016-02806d34a179c855b37cb1f0793d581b-1570773738'
+    //     },
+    //     body: JSON.stringify({
+    //       tokenid: response.id
+    //     })
+    //   })
 
-  // Configuración de la personalización para habilitar solo Mercado Pago Wallet y tarjetas de débito
-  const customization = {
-    visual: {
-      style: {
-        customVariables: {
-          theme: 'flat', // | 'dark' | 'bootstrap' | 'flat'
-          texts: {
-            formTitle: 'Tarjeta de Debito'
-          }
-        }
-      }
-    },
-    paymentMethods: {
-      types: {
-        included: ['credit_card', 'debit_card'] // Excluye tarjetas de crédito
-      },
-      // minInstallments: 1,
-      maxInstallments: 1
-    }
-  }
-
-  // Inicialización del monto que se va a pagar
-  const initialization = {
-    // amount: product.price // 20000
-    amount: 120 // 20000
-    // payer: {
-    //   email: 'correoa_pruebas1111@example.com'
+    //   console.log({ resp })
+    // } catch (error) {
+    //   console.log({ error })
     // }
   }
 
-  // Localización en español para América Latina (Argentina)
-  const locale = 'es-AR'
-
-  // Callback cuando el Brick esté listo
-  const onReady = () => {
-    console.log('Mercado Pago Brick está listo')
-    console.log({ referenceId })
-  }
-
-  // // Callback cuando el usuario haga submit con los datos de pago
-  // const onSubmit = async (paymentData) => {
-  //   console.log('Datos de pago:', paymentData)
-
-  //   try {
-  //     const { data: respuesta } = await axiosInstance.post(
-  //       '/payment/payment-intent',
-  //       paymentData
-  //     )
-  //     console.log({ respuesta })
-  //   } catch (error) {}
-  //   console.log({ error })
-
-  //   // Aquí puedes enviar los datos de pago a tu servidor para procesar la transacción
-  // }
-
-  const onSubmit = async (formData) => {
-    // callback llamado al hacer clic en el botón enviar datos
-    console.log(formData)
-    return new Promise((resolve, reject) => {
-      fetch('http://localhost:3000/api/payment/payment-intent', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Idempotency-Key': idepotency,
-          Authorization: 'Bearer TEST-a01de139-ec21-47ed-8900-1971056920a1'
-        },
-        body: JSON.stringify(formData)
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          // recibir el resultado del pago
-          console.log({ response })
-          resolve()
-        })
-        .catch((error) => {
-          console.log({ error })
-          // manejar la respuesta de error al intentar crear el pago
-          reject()
-        })
-    })
-  }
-
-  const onError = async (error) => {
-    // callback llamado para todos los casos de error de Brick
-    console.log(error)
-  }
-
   return (
-    <div>
-      <h3>Suscripción</h3>
-      <CardPayment
-        initialization={initialization}
-        // customization={customization}
-        // locale={locale}
-        onSubmit={onSubmit}
-        onReady={onReady}
-        onError={onError}
-      />
-    </div>
+    <form onSubmit={onSubmit}>
+      <CardNumber placeholder='Card Number' />
+      <SecurityCode placeholder='Security Code' />
+      <ExpirationDate placeholder='Expiration Date' mode='short' />
+      <button>Pay</button>
+    </form>
   )
 }
 
--- src/utils/initPayMethod.js
+++ src/utils/initPayMethod.js
@@ -3,8 +3,9 @@ import { getDataProduct, getPublicKeyStripe } from './getDataFromAPI'
 import { loadStripe } from '@stripe/stripe-js'
 
 const allpkmp = {
-  pk_test1: 'TEST-a01de139-ec21-47ed-8900-1971056920a1',
-  pk_test2: 'APP_USR-515b7616-d7dd-4a09-b3e6-6cc5127f46c7'
+  pk_test1: 'TEST-a01de139-ec21-47ed-8900-1971056920a1', //onePay
+  pk_test2: 'APP_USR-f9e0aab2-5def-4b4e-9feb-62d7cbbc8ca1', // vendedor 
+  pk_test3: 'TEST-bc5aba84-1864-40ea-b90e-39e4d7bacb3a' //suscription
 }
 
 async function initPayMethod(setStripePromise, setProduct) {
@@ -13,7 +14,7 @@ async function initPayMethod(setStripePromise, setProduct) {
   setProduct(dataProductResp)
 
   //mercado pago
-  initMercadoPago(allpkmp.pk_test1, {
+  initMercadoPago(allpkmp.pk_test2, {
     locale: 'es-AR'
   })
 
